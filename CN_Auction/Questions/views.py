73bd59b1f80b77349fb5f13396c35fdd1c6a71e2
Ritwik Gupta <ritwikg.0501@gmail.com>
2024-04-18 20:03:08 +0530

    Revert "Sends questions from database to website"
    This reverts commit 4e445638a086b75dfdb82265eac3661c3c3d4fc3.

--- CN_Auction/Questions/views.py
+++ CN_Auction/Questions/views.py
@@ -1,18 +1,3 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Question
 
 # Create your views here.
-def send_questions():
-    questions = Question.objects.all()
-    data = []
-    data_format = {}
-    for x in questions:
-        data_format= {
-            'question' : x.question_text,
-            'options' : [x.option_1, x.option_2, x.option_3, x.option_4],
-            'correctAnswer' : x.correct_option
-        }
-        data.append(data_format)
-
-    return JsonResponse({"data" : data, "n" : len(data)})
