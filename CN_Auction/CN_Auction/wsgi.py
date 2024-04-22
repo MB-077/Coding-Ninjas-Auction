a4e2ff1ec79cfcb28164f75f2d6df1d9f29dcb5a
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-04-22 20:45:41 +0530

    Update wsgi.py

--- CN_Auction/CN_Auction/wsgi.py
+++ CN_Auction/CN_Auction/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os, sys
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
 
 
 
@@ -27,5 +27,6 @@ sys.path.append('vir_env\Lib\site-packages')
 # poiting to the project settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CN_Auction.settings")
 
+application = get_wsgi_application()
 # wsgi.py file end
 # ===================
