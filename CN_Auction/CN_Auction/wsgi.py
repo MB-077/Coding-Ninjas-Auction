e91b01943ad246b17a44e6876645ba5d4880f966
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-04-22 20:34:05 +0530

    Update wsgi.py

--- CN_Auction/CN_Auction/wsgi.py
+++ CN_Auction/CN_Auction/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CN_Auction.settings')
-
 application = get_wsgi_application()
+
+
+
+
+# =====================
+# wsgi.py file begin 
+
+
+# add the hellodjango project path into the sys.path
+sys.path.append('CN_Auction\CN_Auction\hellodjango')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('vir_env\Lib\site-packages')
+
+# poiting to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CN_Auction.settings")
+
+# wsgi.py file end
+# ===================
