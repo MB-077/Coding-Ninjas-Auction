575b7e39a9bf2444ed682073b16a7cee7f0e2ba8
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-19 15:55:31 +0530

    updated models.py

--- CN_Auction/Teams/models.py
+++ CN_Auction/Teams/models.py
@@ -10,7 +10,7 @@ class Team(models.Model):
    points_scored = models.IntegerField(default=0)  
 
    def __str__(self):
-      return self.team_name
+      return f"{self.team_id} : {self.team_name}"
 
 
 class Student(models.Model):
@@ -58,14 +58,93 @@ def process_team_data():
          
       # Update Students and Individuals related to the Team
       member_names = [name.strip() for name in df['TEAM_MEMBERS'][i].split(",")]
-      member_rollnos = [rollno.strip() for rollno in df['TEAM_MEMBERS_ROLL_NUMBERS'][i].split(",")]
+      # member_rollnos = [rollno.strip() for rollno in df['TEAM_MEMBERS_ROLL_NUMBERS'][i].split(",")]
 
       for j in range(len(member_names)):
           # Check if the student with the given roll number already exists for this team
-         student, created = Student.objects.get_or_create(student_rollno=member_rollnos[j], team=team)
+         # student, created = Student.objects.get_or_create(student_rollno=member_rollnos[j], team=team)
+         student, created = Student.objects.get_or_create(student_name=member_names[j], team=team)
+         
          # Update or set the student's name
          student.student_name = member_names[j]
          student.save()
+
+      print(f"[  OK  ] Updated data for team: {df['TEAM_NAME'][i]} ...")
+
+   print("\nData Processing Completed Successfully!")
+
+
+def process_team_data_registration():
+   dtype_dict = {
+      "TEAM_ID": int,
+      "TEAM_NAME": str,
+      "NUMBER_OF_MEMBERS": int,
+      "TEAM_MEMBERS": str,
+      "TEAM_MEMBERS_ROLL_NUMBERS": str,
+   }
+
+   # Load the data
+   df = pd.read_csv(r'Teams/Team_Data.csv', dtype=dtype_dict)  # Update path to your CSV
+
+   print("\nProcessing Team Data...\n")
+
+   for g in df['TEAM_ID'].unique():
+      Team.objects.create(team_id=g)
+      
+   # Process the data
+   for i in range(len(df)):
+      # Extract player information from the DataFrame
+      team_id = df.at[i, "TEAM_ID"]
+      team_name = df.at[i, "TEAM_NAME"]
+      
+      # Update Team attributes
+      team = Team.objects.get(team_id=team_id)
+      team.team_name = team_name
+      team.save()
+         
+      # Update Students and Individuals related to the Team
+      member_names = [name.strip() for name in df['TEAM_MEMBERS'][i].split(",")]
+      # member_rollnos = [rollno.strip() for rollno in df['TEAM_MEMBERS_ROLL_NUMBERS'][i].split(",")]        
+         
+      for j in range(len(member_names)):
+          # Check if the student with the given roll number already exists for this team
+         # student, created = Student.objects.get_or_create(student_rollno=member_rollnos[j], team=team)
+         student, created = Student.objects.get_or_create(student_name=member_names[j], team=team)
+         # Update or set the student's name
+         student.student_name = member_names[j]
+         student.student_rollno = None
+         student.save()
+
+      print(f"[  OK  ] Updated data for team: {df['TEAM_NAME'][i]} ...")
+
+   print("\nData Processing Completed Successfully!")
+   
+
+def process_team_data_purse():
+   dtype_dict = {
+      "TEAM_ID": int,
+      "TEAM_NAME": str,
+      "CORRECT_ANSWERS": int,
+      "PURSE_VALUE": int,
+   }
+
+   # Load the data
+   df = pd.read_csv(r'Teams/Team_Data.csv', dtype=dtype_dict)  # Update path to your CSV
+
+   print("\nProcessing Team Data...\n")
+      
+   # Process the data
+   for i in range(len(df)):
+      # Extract player information from the DataFrame
+      team_id = df.at[i, "TEAM_ID"]
+      correct_answers = df.at[i, "CORRECT_ANSWERS"]
+      purse_value = df.at[i, "PURSE_VALUE"]
+      
+      # Update Team attributes
+      team = Team.objects.get(team_id=team_id)
+      team.correct_answers = correct_answers
+      team.purse_value = purse_value
+      team.save()
 
       print(f"[  OK  ] Updated data for team: {df['TEAM_NAME'][i]} ...")
 
